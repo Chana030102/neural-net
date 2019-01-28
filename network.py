bc01e6e61ede352e36cfdd2a7d8eebbab92b3a1a
Chana030102 <chanaar@pdx.edu>
2019-01-28 14:23:31 -0800

    Add confusion matrix updating

--- network.py
+++ network.py
@@ -26,7 +26,7 @@ class NeuralNet:
         self.out_buffer = [None]*num_outputs
 
         # table to keep track of accuracy for evalutations
-        self.accuracy = pandas.DataFrame(columns=range['test_c','test_i','train_c','train_i'])
+        self.accuracy = pandas.DataFrame(columns=['test_c','test_i','train_c','train_i'])
         self.epoch = 0 # count number of times trained
 
         # confusion matrix for evaluation 
@@ -50,6 +50,27 @@ class NeuralNet:
                 self.accuracy[set_name + '_c'][self.epoch] += 1
             else:
                 self.accuracy[set_name + '_i'][self.epoch] += 1
+        
+    # run network through provided data and add to confusion matrix
+    # set name will be "train" or "test" for data set identification
+    def final_evaluate(self, input_data, targets, set_name):
+        # loop for each row of data
+        for data_index in range(0,len(input_data)):
+            # loop for each node in hidden layer
+            for node_index in range(0,self.size_hidden_layer):
+                self.hid_buffer[node_index] = self.hidden_layer[node_index].evaluate(input_data[data_index])
+
+            # loop for each node in output layer
+            for node_index in range(0,self.size_output_layer):
+                self.out_buffer[node_index] = self.output_layer[node_index].evaluate(self.hid_buffer)
+
+            # update accuracy table
+            if (targets[data_index] == self.out_buffer.index(max(self.out_buffer))):
+                self.accuracy[set_name + '_c'][self.epoch] += 1
+            else:
+                self.accuracy[set_name + '_i'][self.epoch] += 1
+            
+            self.c_matrix[targets[data_index]][self.out_buffer.index(max(self.out_buffer))] += 1
 
     def train(self, input_data, targets):
          # loop through each row of data
