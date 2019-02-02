fa198c49aa596ae02437ed9cb5ae7145347097f1
Chana030102 <chanaar@pdx.edu>
2019-02-02 10:52:02 -0800

    Add implementation for network forward propagation

--- network.py
+++ network.py
@@ -6,7 +6,7 @@
 #
 # 
 
-import math, numpy
+import math, numpy, pandas
 
 WEIGHT_LOW = -0.05
 WEIGHT_HIGH = 0.05
@@ -15,6 +15,7 @@ sigmoid = lambda x : 1/(1+math.exp(-x))
 
 class NeuralNet:
 
+    # Input number should not include bias
     def __init__(self, num_inputs, num_hidden_nodes, num_outputs, momentum, learn_rate):
         self.size_inputs = num_inputs + 1
         self.size_hidden = num_hidden_nodes + 1
@@ -35,12 +36,25 @@ class NeuralNet:
         self.weight_hidden_to_out = numpy.random.uniform(low=WEIGHT_LOW,high=WEIGHT_HIGH,size=(self.size_hidden,self.size_output))
         self.weight_hidden_to_out_prevdelta = numpy.zeros((self.size_hidden,self.size_output))
 
+        self.accuracy = pandas.DataFrame(0,index=range(0,51),columns=['test_c','test_i','train_c','train_i'])
+        self.c_matrix = pandas.DataFrame(0,index=range(0,self.size_output),columns=range(0,self.size_output))
+
     # Forward propagation activation for network
-    def activation(self, input_data):
-        
+    def activation(self, input_data, return_hidden=False):
+        i_to_h_dot = numpy.dot(self.weight_input_to_hidden,[1]+input_data.transpose())
+        hidden_activations = numpy.asarray(list(map(sigmoid,i_to_h_dot)))
+
+        h_to_o_dot = numpy.dot(self.weight_hidden_to_out,[1]+hidden_activations)
+        out_activations = numpy.asarray(list(map(sigmoid,h_to_o_dot)))
+
+        if(return_hidden == False):
+            return [hidden_activations,out_activations]
+        else:
+            return out_activations
 
     # Back propagation to update weights in network
-    def updateWeights(self, input_data):
+    def updateWeights(self, input_data, hidden_activations, out_activations, target):
+        
 
     # Calculate activation for inputs and record accuracy
     def evaluate(self, input_data):
@@ -53,4 +67,3 @@ class NeuralNet:
 
     # Output confusion matrix
     def report_matrix(self, file_name):
-        
