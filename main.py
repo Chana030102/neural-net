a6a2cbb6d6e886378991cf3f26fd692bfd04d388
Chana030102 <chanaar@pdx.edu>
2019-03-20 10:01:12 -0700

    Added directory creation for organization

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 
 import numpy as np 
 import pandas, pickle
-import network, sys
+import network, sys, os
 
 np.seterr(all='print')
 NAME      = input("Enter name of experiment: ")
@@ -26,7 +26,7 @@ else:
     momentum = 0.9
     learning_rate = 0.1
 
-TITLE = "{}_h={}_m={}_lr={}".format(NAME,hidden_layer_size,momentum,learning_rate)
+PATH = "{}_h={}_m={}_lr={}/".format(NAME,hidden_layer_size,momentum,learning_rate)
 
 TRAIN_FILE = "../mnist_train.csv"
 TEST_FILE  = "../mnist_test.csv"
@@ -38,6 +38,10 @@ output_layer_size = 10
 MAX_EPOCH = 2 
 INPUT_MAX = 255
 increment = 1000
+
+# Make directory if it doesn't exist
+if os.path.exists(PATH) == False:
+    os.mkdir(PATH)
 
 # Import data
 traind = np.loadtxt(TRAIN_FILE,delimiter=DELIMITER)
@@ -59,10 +63,10 @@ t1 = int(len(traind)/increment)
 t2 = int(len(testd)/increment)
 
 for i in range(t1):
-    pickle.dump(traind[(i*increment):((i+1)*increment)],open(TITLE_TRAIN+str(i),'wb'))
+    pickle.dump(traind[(i*increment):((i+1)*increment)],open(PATH+TITLE_TRAIN+str(i),'wb'))
 
 for i in range(t2):
-    pickle.dump(testd[(i*increment):((i+1)*increment)],open(TITLE_TEST+str(i),'wb'))
+    pickle.dump(testd[(i*increment):((i+1)*increment)],open(PATH+TITLE_TEST+str(i),'wb'))
 
 input_size = len(testd[0]) # how many inputs are in one row
 del testd,traind
@@ -75,19 +79,19 @@ for e in range(MAX_EPOCH):
     
     for i in range(t1):
         print("Train file: {}".format(i))
-        traind = pickle.load(open(TITLE_TRAIN+str(i),'rb'))
+        traind = pickle.load(open(PATH+TITLE_TRAIN+str(i),'rb'))
         net.evaluate(TITLE_TRAIN,traind,trainl[i*increment:(i+1)*increment])
         del traind
     
     for i in range(t2):
         print("Test file: {}".format(i))
-        testd = pickle.load(open(TITLE_TEST+str(i),'rb'))
+        testd = pickle.load(open(PATH+TITLE_TEST+str(i),'rb'))
         net.evaluate(TITLE_TEST,testd,testl[i*increment:(i+1)*increment])
         del testd
     
     for i in range(t1):
         print("Train file: {}".format(i))
-        traind = pickle.load(open(TITLE_TRAIN+str(i),'rb'))
+        traind = pickle.load(open(PATH+TITLE_TRAIN+str(i),'rb'))
         net.train(traind,trainl[i*increment:(i+1)*increment])
         del traind
 
@@ -97,10 +101,10 @@ for e in range(MAX_EPOCH):
 # Create confusion matrix for test data testing
 for i in range(t2):
     print("Test file: {}".format(i))
-    testd = pickle.load(open(TITLE_TEST+str(i),'rb'))
+    testd = pickle.load(open(PATH+TITLE_TEST+str(i),'rb'))
     net.evaluate(TITLE_TEST,testd,testl[i*increment:(i+1)*increment],True)
     net.train(testd,testl[i*increment:(i+1)*increment])
     del testd
 
-net.report_accuracy(TITLE)
-net.report_confusion_matrix(TITLE)
+net.report_accuracy(PATH+NAME)
+net.report_confusion_matrix(PATH+NAME)
